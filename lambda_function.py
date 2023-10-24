75febe8fc44d29ace61093494e967fb093b40ee4
Estefany Quezada <estefanyquezadab@gmail.com>
2023-10-24 14:10:02 -0300

    solucion bastian

--- lambda_function.py
+++ lambda_function.py
@@ -1,62 +1,58 @@
 import json
 import psycopg2
-from psycopg2 import sql
 import os
+from psycopg2 import sql
 
-
-def lambda_handler(event):
+def lambda_handler(event, context):
     db_host = os.environ['DB_HOST']
-    db_port = os.environ['DB_PORT']
     db_name = os.environ['DB_NAME']
     db_user = os.environ['DB_USER']
     db_password = os.environ['DB_PASSWORD']
-
+    db_port = os.environ['DB_PORT']
 
     try:
-        conn = psycopg2.connect(
-            host = db_host,
-            port = db_port,
-            database = db_name,
-            user = db_user,
-            password = db_password
-        )
-        body=json.loads(event['body'])
-        nombre = body['nombre']
-        tipo = body['tipo']
-        region = body['region']
-
+        conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host, port=db_port)
+        print("Connected to database")
         cursor = conn.cursor()
-        insert = sql.SQL("INSERT INTO POKEMON (nombre) values ({})returning id;").format(sql.Literal(nombre))
+        body = json.loads(event['body'])
+        nombre = body['nombre_pokemon']
+        tipo = body['tipo_pokemon']
+        region = body['region_pokemon']
+
+        insert = sql.SQL("INSERT INTO pokemon (nombre_pokemon) VALUES ({}) returning id").format(sql.Literal(nombre))
         cursor.execute(insert)
-        id_pokemon= cursor.fetchone()[0]
-        conn.commit()
-        insert = sql.SQL("INSERT INTO TIPO (tipo,id_pokemon) values ({},{})returning id;").format(sql.Literal(tipo,id_pokemon),sql.Literal(id_pokemon))
+        id_pokemon = cursor.fetchone()[0]
+        conn.commit()  # Agregué los paréntesis aquí
+
+        insert = sql.SQL("INSERT INTO region (nombre_region) VALUES ({}) returning id").format(sql.Literal(region))
         cursor.execute(insert)
-        conn.commit()
-        insert = sql.SQL("INSERT INTO REGION (region,id_pokemon) values ({},{})returning id;").format(sql.Literal(region),sql.Literal(id_pokemon))
+        conn.commit()  # Agregué los paréntesis aquí
+        id_region = cursor.fetchone()[0]
+
+        insert = sql.SQL("INSERT INTO tipo (nombre_tipo) VALUES ({}) returning id").format(sql.Literal(tipo))
         cursor.execute(insert)
-        conn.commit()
+        conn.commit()  # Agregué los paréntesis aquí
+        id_tipo = cursor.fetchone()[0]
+
+        insert = sql.SQL("INSERT INTO region_pokemon (id_region, id_pokemon) VALUES ({}, {}) returning id").format(sql.Literal(id_region), sql.Literal(id_pokemon))
+        cursor.execute(insert)
+        conn.commit()  # Agregué los paréntesis aquí
+
+        insert = sql.SQL("INSERT INTO tipo_pokemon (id_tipo, id_pokemon) VALUES ({}, {}) returning id").format(sql.Literal(id_tipo), sql.Literal(id_pokemon))
+        cursor.execute(insert)
+        conn.commit()  # Agregué los paréntesis aquí
 
         cursor.close()
         conn.close()
 
         response = {
             "statusCode": 200,
-            "body":"Pokemon insertado correctamente" 
+            "body": "Pokemon insertado correctamente"
         }
-
-    except psycopg2.DatabaseError as e:
+    except Exception as e:
         response = {
-        "statusCode": 500,
-            "body":str(e)
+            "statusCode": 500,
+            "body": str(e)
         }
-    
     return response
-    
-postmanPokemon = {
-    "nombre":"Bulbasur",
-    "tipo": "1",
-    "region": "1"
-}
-print(lambda_handler(postmanPokemon))
 
